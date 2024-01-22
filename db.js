541608d2883d8e1a96e31f113fcaa71916ec0001
Karishma12022 <jainkarishma1202@gmail.com>
2024-01-22 23:36:50 +0530

    First Commit

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 function connect () {
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+    // mongoose.set('useCreateIndex', true);
+    mongoose.connect('mongodb://localhost:27017/complaint')
 }
 
 module.exports = connect
